ac56da3e3a97a6eb02527ae880f7b5505a02fbd0
Joeljoy1237 <joeljoy1237@gmail.com>
2025-02-19 22:08:13 +0530

    fix : dashboard overlay issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -69,7 +69,7 @@ const Navbar = () => {
 
       {/* Mobile Sidebar */}
       <div
-        className={`fixed top-0 right-0 h-full w-3/4 max-w-sm bg-white shadow-lg transform transition-transform duration-300 ease-in-out ${
+        className={`fixed top-0 right-0 h-full w-3/4 max-w-sm bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 ${
           isSidebarOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
