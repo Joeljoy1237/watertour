506eec3a9bebb46ca0b9559d1d382749fefd0396
Deepak <deepakd7356.com>
2025-03-27 14:58:18 +0530

    fix: something

--- src/app/(root)/book/[id]/page.tsx
+++ src/app/(root)/book/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 import HouseboatCarousel from "@/components/HouseboatCarousel";
 import CommentSection from "@/components/CommentSection";
 
@@ -91,7 +91,7 @@ const nonVegItems = [
             The houseboat features a well-furnished bedroom, a private bathroom with a bathtub, and a dining area with a TV. A music system adds to the ambiance and air conditioning is available.
             <br />
             Safety and convenience are prioritized, making this houseboat a great choice for a romantic getaway or a peaceful retreat. Freshly prepared Kerala cuisine enhances the experience, allowing guests to enjoy traditional flavors while gliding through the backwaters.
-            <br /><br />
+            <br />
             Book a stay today and enjoy the beauty of Alleppey&apos;s waterways for an unforgettable journey.
             </p>
           </div>
