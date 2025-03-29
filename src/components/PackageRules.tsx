7f772fc391c704517eb60defd0de660b06cb66ed
Joeljoy1237 <joeljoy1237@gmail.com>
2025-03-29 10:08:47 +0530

    fix: change head

---
+++ src/components/PackageRules.tsx
@@ -0,0 +1,22 @@
+<div className="bg-white p-6 rounded-md shadow-md">
+    <h2 className="text-2xl font-semibold text-primary-600">
+       Luxury Houseboat Stay
+    </h2>
+        <p className="text-gray-700 mt-2">
+        • Check in Time: 12 noon and Checkout 09.00 AM next day. <br />
+        • Houseboats are for exclusive usage and not shared. <br />
+        • Houseboats are accompanied by navigator and cook. <br />
+        • Meal plan - Starts with Lunch and ends with Breakfast (Typical Kerala Cuisine, if you require any other cuisine inform 
+          us at least 5 days in advance) <br />
+        • The Houseboat will anchor by 5:30 PM for the night stay and will retain the cruise the very next morning at 08:00 AM. 
+          There is no cruising in the lake allowed in the evening hours or late night. <br />
+        • You can use A/C anytime required in PREMIUM / LUXURY Houseboats. <br />
+        • Swimming in the backwater is strictly prohibited. <br />
+        • Do not cause noise disturbance to the villagers during night halt. <br />
+        • The Food and beverage service on the houseboat is only up to 10:30 PM. <br />
+        • Guests are not permitted to remain or sleep outside the rooms after 10:30 PM. <br />
+        • Boat will be halted for one hour during lunch break. <br />
+        • The distance we cover is about 20-30 kms and the place of the night halt will be decided by the captain according to 
+          the weather and climate
+        </p>
+</div>
