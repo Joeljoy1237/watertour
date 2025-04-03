61e9a419418db425f1a0738c8b673ab57a6152e3
Joeljoy1237 <joeljoy1237@gmail.com>
2025-04-03 14:12:07 +0530

    refactor: streamline NextAuth configuration and enhance user session handling

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,25 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth, { NextAuthOptions, AdapterUser } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import CryptoJS from "crypto-js";
 import User from "@/models/User"; // Adjust the path based on your User model location
 import { connectToDB } from "@/utils/database"; // Adjust the path based on your DB connection utility
-import Credentials from "next-auth/providers/credentials";
-// Extend NextAuth Session & JWT Types
+
+// Extend NextAuth Session, JWT, & AdapterUser Types
 declare module "next-auth" {
     interface Session {
         user: {
             id: string;
             email: string;
             name: string;
-            image: string; // Add image URL
-            phone?: string; // Add phone number
+            image?: string;
+            phone?: string;
             isAdmin: boolean;
             isOwner: boolean;
         };
     }
-    interface User {
-        given_name?: string;
-        family_name?: string;
+
+    interface AdapterUser {
         isOwner: boolean;
-        email: string;
-        image?: string; // Add image URL
-        picture?: string; // Add picture URL
     }
 }
 
@@ -31,94 +27,58 @@ declare module "next-auth/jwt" {
     interface JWT {
         id: string;
         email: string;
-        firstName: string;
-        lastName: string;
-        image?: string; // Add image URL
-        phone?: string; // Add phone number
+        name: string;
+        image?: string;
+        phone?: string;
         isOwner: boolean;
         isAdmin?: boolean;
     }
 }
 
-// Function to fetch phone number from Google People API
-async function fetchPhoneNumber(accessToken: string) {
-    const response = await fetch('https://people.googleapis.com/v1/people/me?personFields=phoneNumbers', {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
-    const data = await response.json();
-    return data.phoneNumbers ? data.phoneNumbers[0].value : null; // Return the phone number if available
-}
-
 // NextAuth Configuration
 const authOptions: NextAuthOptions = {
     providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-            authorization: {
-                params: {
-                    scope: "openid profile email phone",
-                },
-            },
-        }),
         Credentials({
             credentials: {
-              email: { label: "Email", type: "text" },
-              password: { label: "Password", type: "password" },
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" },
             },
             async authorize(credentials) {
                 await connectToDB();
-            
+
                 if (!credentials) {
                     throw new Error(JSON.stringify({ message: "Credentials not provided", desc: "Please provide both email and password" }));
                 }
-            
+
                 try {
                     const userExist = await User.findOne({ email: credentials.email });
-            
+
                     if (!userExist) {
-                        console.log("User does not exist");
                         throw new Error(JSON.stringify({ message: "User does not exist", desc: "Please check the email and try again" }));
                     }
-                    console.log(userExist);
 
-                    // Check if the password exists before attempting to decrypt it
                     if (!userExist.password) {
                         throw new Error(JSON.stringify({ message: "Password is missing", desc: "The password is not available for this user" }));
                     }
-            
+
                     const bytes = CryptoJS.AES.decrypt(userExist.password, process.env.CRYPTO_SECRET_KEY!);
-            
-                    // Check if decryption was successful
                     const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
-            
-                    if (!decryptedData) {
-                        console.log("Decryption failed or the password is incorrect");
+
+                    if (!decryptedData || decryptedData !== credentials.password) {
                         throw new Error(JSON.stringify({ message: "Email or Password is not correct", desc: "Please check your credentials and try again" }));
                     }
-            
-                    const isMatch = decryptedData === credentials.password;
-                    console.log(isMatch);
-            
-                    if (isMatch) {
-                        console.log(userExist);
-                        return userExist; // Return user object if authentication is successful
-                    } else {
-                        console.log("Email or Password is not correct");
-                        throw new Error(JSON.stringify({ message: "Email or Password is not correct", desc: "Please check your credentials and try again" }));
+
+                    return userExist; // Return user object if authentication is successful
+                } catch (err: unknown) {
+                    if (err instanceof Error) {
+                        console.error(err.message);
+                        throw new Error(JSON.stringify({ message: "Internal Server Error", desc: "An unexpected error occurred. Please try again later." }));
                     }
-                } catch (err: any) {
-                    console.log(err);
+                    console.error("An unknown error occurred");
                     throw new Error(JSON.stringify({ message: "Internal Server Error", desc: "An unexpected error occurred. Please try again later." }));
                 }
-            }
-            ,
-          }),
-      
-        
-
+            },
+        }),
     ],
     callbacks: {
         async session({ session, token }) {
@@ -134,47 +94,15 @@ const authOptions: NextAuthOptions = {
             };
             return session;
         },
-        async jwt({ token, user, account }) {
+        async jwt({ token, user }) {
             if (user) {
                 token.id = user.id;
-                token.email = user.email;
-                token.isOwner = user.isOwner;
-                token.nameame = user.name ?? "";
-                token.image = user.image;
-
-                // Fetch phone number if the provider is Google
-                if (account!.provider === "google") {
-                    const phoneNumber = await fetchPhoneNumber(account!.access_token!);
-                    token.phoneNumber = phoneNumber; // Store phone number in token
-                }
+                token.email = user.email ?? "";
+                token.isOwner = (user as unknown as AdapterUser).isOwner;
+                token.name = user.name ?? "";
+                token.image = user.image ?? undefined;
             }
             return token;
-        },
-        async signIn({ user, account }) {
-
-            try {
-                await connectToDB(); // Ensure DB connection
-                const existingUser = await User.findOne({ email: user.email });
-
-                if (!existingUser) {
-                    let phoneNumber: string | null = null;
-                    if (account!.provider === "google") {
-                        phoneNumber = await fetchPhoneNumber(account!.access_token!);
-                    }
-
-                    const newUser = new User({
-                        name: user.name,
-                        email: user.email,
-                        image: user.picture,
-                        phoneNumber: phoneNumber,
-                    });
-                    await newUser.save();
-                }
-                return true; // Allow sign-in
-            } catch (error) {
-                console.error("Error during sign-in:", error);
-                return false; // Prevent sign-in on error
-            }
         },
     },
     secret: process.env.NEXTAUTH_SECRET,
--- src/app/api/owner/approveBooking/route.ts
+++ src/app/api/owner/approveBooking/route.ts
@@ -5,167 +5,158 @@ import User from "@/models/User";
 import Houseboat from "@/models/Houseboat";
 import nodemailer from "nodemailer";
 export async function POST(req: NextRequest) {
-    try {
-        const { userId, bookingId } = await req.json();
+  try {
+    const { userId, bookingId } = await req.json();
 
-        if (!bookingId) {
-            return NextResponse.json({ error: "Booking ID is required" }, { status: 400 });
+    if (!bookingId) {
+      return NextResponse.json({ error: "Booking ID is required" }, { status: 400 });
+    }
+
+    // Connect to MongoDB (if not already connected)
+    if (mongoose.connection.readyState !== 1) {
+      await mongoose.connect(process.env.MONGODB_URI!);
+    }
+
+    // Find and update the booking status
+    const updatedBooking = await Booking.findByIdAndUpdate(
+      bookingId,
+      { status: "approved", updatedAt: new Date() },
+      { new: true }
+    );
+
+    if (!updatedBooking) {
+      return NextResponse.json({ error: "Booking not found" }, { status: 404 });
+    }
+
+    // Update the houseboat's availability
+    const houseboat = await Houseboat.findById(updatedBooking.houseboatId);
+    if (houseboat) {
+      const dateData = houseboat.dates.get(updatedBooking.date);
+      if (dateData) {
+        if (updatedBooking.type === "Day Cruiser") {
+          dateData.dayCruiser = false;
+          dateData.dayCruiserBooked = true;
+        } else {
+          dateData.nightStay = false;
+          dateData.nightStayBooked = true;
         }
+        await houseboat.save();
+      }
+    }
 
-        // Connect to MongoDB (if not already connected)
-        if (mongoose.connection.readyState !== 1) {
-            await mongoose.connect(process.env.MONGODB_URI!);
-        }
-
-        // Find and update the booking status
-        const updatedBooking = await Booking.findByIdAndUpdate(
-            bookingId,
-            { status: "approved", updatedAt: new Date() },
-            { new: true }
-        );
-
-        if (!updatedBooking) {
-            return NextResponse.json({ error: "Booking not found" }, { status: 404 });
-        }
-
-        // Update the houseboat's availability
-        const houseboat = await Houseboat.findById(updatedBooking.houseboatId);
-        if (houseboat) {
-            const dateData = houseboat.dates.get(updatedBooking.date);
-            if (dateData) {
-                if (updatedBooking.type === "Day Cruiser") {
-                    dateData.dayCruiser = false;
-                    dateData.dayCruiserBooked = true;
-                } else {
-                    dateData.nightStay = false;
-                    dateData.nightStayBooked = true;
-                }
-                await houseboat.save();
-            }
-        }
-
-        const user = await User.findById({ _id: userId }).select("revenue");
-        user.revenue = user.revenue + updatedBooking.totalPrice;
-        await user.save();
+    const user = await User.findById({ _id: userId }).select("revenue");
+    user.revenue = user.revenue + updatedBooking.totalPrice;
+    await user.save();
 
         
     
 
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
+    // // const transporter = nodemailer.createTransport({
+    // //   service: "Gmail",
+    // //   auth: {
+    // //     user: process.env.EMAIL,
+    // //     pass: process.env.EMAIL_PASSWORD,
+    // //   },
+    // // });
 
 
-  try {
-    const mailOptions = {
-      from: process.env.EMAIL,
-      to: updatedBooking.email,
-      subject: "Water Tour Booking Confirmation",
-      html: `
-        <html>
-        <head>
-          <style>
-            body {
-              font-family: Arial, sans-serif;
-              line-height: 1.6;
-              color: #333;
-              margin: 0;
-              padding: 0;
-            }
-            .container {
-              max-width: 600px;
-              margin: 0 auto;
-              padding: 20px;
-            }
-            .header {
-              background-color: #2ca01c;
-              color: white;
-              padding: 20px;
-              text-align: center;
-            }
-            .content {
-              padding: 20px;
-              background-color: #f9f9f9;
-            }
-            .booking-details {
-              background-color: white;
-              padding: 15px;
-              border-radius: 5px;
-              margin: 20px 0;
-            }
-            .footer {
-              text-align: center;
-              padding: 20px;
-              font-size: 14px;
-              color: #666;
-            }
-          </style>
-        </head>
-        <body>
-          <div class="container">
-            <div class="header">
-              <h1>Booking Confirmed!</h1>
-            </div>
-            <div class="content">
-              <p>Dear Guest,</p>
-              <p>Your houseboat booking has been confirmed. Here are your booking details:</p>
+    // //   try {
+    //     const mailOptions = {
+    //       from: process.env.EMAIL,
+    //       to: updatedBooking.email,
+    //       subject: "Water Tour Booking Confirmation",
+    //       html: `
+    //         <html>
+    //         <head>
+    //           <style>
+    //             body {
+    //               font-family: Arial, sans-serif;
+    //               line-height: 1.6;
+    //               color: #333;
+    //               margin: 0;
+    //               padding: 0;
+    //             }
+    //             .container {
+    //               max-width: 600px;
+    //               margin: 0 auto;
+    //               padding: 20px;
+    //             }
+    //             .header {
+    //               background-color: #2ca01c;
+    //               color: white;
+    //               padding: 20px;
+    //               text-align: center;
+    //             }
+    //             .content {
+    //               padding: 20px;
+    //               background-color: #f9f9f9;
+    //             }
+    //             .booking-details {
+    //               background-color: white;
+    //               padding: 15px;
+    //               border-radius: 5px;
+    //               margin: 20px 0;
+    //             }
+    //             .footer {
+    //               text-align: center;
+    //               padding: 20px;
+    //               font-size: 14px;
+    //               color: #666;
+    //             }
+    //           </style>
+    //         </head>
+    //         <body>
+    //           <div class="container">
+    //             <div class="header">
+    //               <h1>Booking Confirmed!</h1>
+    //             </div>
+    //             <div class="content">
+    //               <p>Dear Guest,</p>
+    //               <p>Your houseboat booking has been confirmed. Here are your booking details:</p>
               
-              <div class="booking-details">
-                <p><strong>Booking ID:</strong> ${updatedBooking._id}</p>
-                <p><strong>Houseboat:</strong> ${updatedBooking.houseboatId.name}</p>
-                <p><strong>Date:</strong> ${updatedBooking.date}</p>
-                <p><strong>Type:</strong> ${updatedBooking.type}</p>
-                <p><strong>Number of Guests:</strong> ${updatedBooking.guests}</p>
-                ${updatedBooking.type === 'Night Stay' ? `<p><strong>Number of Beds:</strong> ${updatedBooking.beds}</p>` : ''}
-                <p><strong>Total Amount:</strong> ₹${updatedBooking.totalPrice}</p>
-              </div>
+    //               <div class="booking-details">
+    //                 <p><strong>Booking ID:</strong> ${updatedBooking._id}</p>
+    //                 <p><strong>Houseboat:</strong> ${updatedBooking.houseboatId.name}</p>
+    //                 <p><strong>Date:</strong> ${updatedBooking.date}</p>
+    //                 <p><strong>Type:</strong> ${updatedBooking.type}</p>
+    //                 <p><strong>Number of Guests:</strong> ${updatedBooking.guests}</p>
+    //                 ${updatedBooking.type === 'Night Stay' ? `<p><strong>Number of Beds:</strong> ${updatedBooking.beds}</p>` : ''}
+    //                 <p><strong>Total Amount:</strong> ₹${updatedBooking.totalPrice}</p>
+    //               </div>
 
-              <p>Please keep this email for your records. If you have any questions or need to make changes to your booking, please contact us.</p>
+    //               <p>Please keep this email for your records. If you have any questions or need to make changes to your booking, please contact us.</p>
               
-              <p>We look forward to providing you with an amazing houseboat experience!</p>
-            </div>
-            <div class="footer">
-              <p>Thank you for choosing Water Tour</p>
-              <p>For support, contact: support@watertour.com</p>
-            </div>
-          </div>
-        </body>
-        </html>
-      `
-    };
+    //               <p>We look forward to providing you with an amazing houseboat experience!</p>
+    //             </div>
+    //             <div class="footer">
+    //               <p>Thank you for choosing Water Tour</p>
+    //               <p>For support, contact: support@watertour.com</p>
+    //             </div>
+    //           </div>
+    //         </body>
+    //         </html>
+    //       `
+    //     };
 
-    await transporter.sendMail(mailOptions).catch((error: Error) => {
-      console.error("Error sending email:", error);
-      return new NextResponse(
-        JSON.stringify({
-          message: "Failed to send Approval email",
-          desc: "There was an error sending the email. Please try again later or contact support.",
-        }),
-        { status: 500 }
-      );
-    });
+    //     await transporter.sendMail(mailOptions).catch((error: Error) => {
+    //       console.error("Error sending email:", error);
+    //       return new NextResponse(
+    //         JSON.stringify({
+    //           message: "Failed to send Approval email",
+    //           desc: "There was an error sending the email. Please try again later or contact support.",
+    //         }),
+    //         { status: 500 }
+    //       );
+    //     });
 
     return new NextResponse(JSON.stringify({ message: "Email sent successfully" }), {
       status: 200,
     });
-  } catch (err: any) {
+  } catch (err: unknown) {
     console.error("Internal Server Error:", err);
     return new NextResponse(
       JSON.stringify({ message: "Internal Server Error" }),
       { status: 500 }
     );
   }
-
-
-
-
-        return NextResponse.json(updatedBooking, { status: 200 });
-    } catch (error) {
-        console.error("Error approving booking:", error);
-        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-    }
 }
